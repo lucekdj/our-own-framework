80d3b1a7a6feb850c8688058cdc0c95485b01168
lucekdj <lucekdj@gmail.com>
2023-04-21 18:59:32 -0500

    first own framework

---
+++ src/test/java/utilities/DriverUtils.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverUtils {
+    static WebDriver driver ;
+    public static void createDriver(){
+        
+    }
+
+}
