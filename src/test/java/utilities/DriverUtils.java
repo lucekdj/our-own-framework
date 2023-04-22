6bfbcd83bdf6ff14ffc641885837816aea023bcd
paulinagora88 <paulinagora@yahoo.com>
2023-04-21 20:47:02 -0500

    Commit to master added getDriver method

--- src/test/java/utilities/DriverUtils.java
+++ src/test/java/utilities/DriverUtils.java
@@ -6,14 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class DriverUtils {
-    static WebDriver driver ;
-    public static void createDriver(){
+    static WebDriver driver;
+
+    public static void createDriver() {
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get("http://18.118.14.155:8080/bank/login");
     }
 
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
 }
+
