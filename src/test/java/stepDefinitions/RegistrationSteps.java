9ff5006170f3814dcbecc561091da56bfa78bd83
paulinagora88 <paulinagora@yahoo.com>
2023-05-01 13:25:13 -0500

    registrationfeature

--- src/test/java/stepDefinitions/RegistrationSteps.java
+++ src/test/java/stepDefinitions/RegistrationSteps.java
@@ -24,5 +24,13 @@ public class RegistrationSteps {
     }
 
 
+    Feature: Register user feature
+
+    Scenario: Verify user is able to register for account
+    Given user clicks on sign up here button
+    When user fills out first registration page
+    Then user fills out second registration page
+    Then verify registration is successful
+
 
 }
