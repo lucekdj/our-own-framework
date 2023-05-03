c245e61cbcdcf082576b5d80fda8fae485e7ff6e
paulinagora88 <paulinagora@yahoo.com>
2023-05-03 10:08:39 -0500

    first commit

---
+++ src/test/java/pages/NewCheckingAccountPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NewCheckingAccountPage {
+
+    @FindBy(css=".card-title")
+    WebElement NewCheckingAcctPage;
+
+
+
+
+}
